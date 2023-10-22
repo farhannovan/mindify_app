dc4d10e6b0a4d5d53bcf1e2e074bbb51f5e79162
farhannovan <farhannovan@gmail.com>
2023-10-22 14:19:14 +0700

    refactor

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,457 @@
 import 'package:flutter/material.dart';
-import 'package:mindify_app/screens/home.dart';
+import 'package:intl/intl.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.light(
+          primary: Colors.blue,
+        ),
+      ),
       debugShowCheckedModeBanner: false,
       home: HomeScreen(),
     );
   }
 }
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<Note> filteredNotes = [];
+  bool sorted = false;
+
+  @override
+  void initState() {
+    super.initState();
+    filteredNotes = sampleNotes;
+  }
+
+  List<Note> sortNotesByModifiedTime(List<Note> notes) {
+    if (sorted) {
+      notes.sort((a, b) => a.modifiedTime.compareTo(b.modifiedTime));
+    } else {
+      notes.sort((b, a) => a.modifiedTime.compareTo(b.modifiedTime));
+    }
+
+    sorted = !sorted;
+
+    return notes;
+  }
+
+  void onSearchTextChanged(String searchText) {
+    setState(() {
+      filteredNotes = sampleNotes
+          .where((note) =>
+      note.content.toLowerCase().contains(searchText.toLowerCase()) ||
+          note.title.toLowerCase().contains(searchText.toLowerCase()))
+          .toList();
+    });
+  }
+
+  void deleteNote(int index) {
+    setState(() {
+      Note note = filteredNotes[index];
+      sampleNotes.remove(note);
+      filteredNotes.removeAt(index);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey.shade900,
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(16, 40, 16, 0),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  'Mindify',
+                  style: TextStyle(fontSize: 30, color: Colors.white),
+                ),
+                IconButton(
+                    onPressed: () {
+                      setState(() {
+                        filteredNotes = sortNotesByModifiedTime(filteredNotes);
+                      });
+                    },
+                    padding: const EdgeInsets.all(0),
+                    icon: Container(
+                      width: 40,
+                      height: 40,
+                      decoration: BoxDecoration(
+                          color: Colors.grey.shade800.withOpacity(.8),
+                          borderRadius: BorderRadius.circular(10)),
+                      child: const Icon(
+                        Icons.sort,
+                        color: Colors.white,
+                      ),
+                    ))
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            TextField(
+              onChanged: onSearchTextChanged,
+              style: const TextStyle(fontSize: 16, color: Colors.white),
+              decoration: InputDecoration(
+                contentPadding: const EdgeInsets.symmetric(vertical: 12),
+                hintText: "Search notes...",
+                hintStyle: const TextStyle(color: Colors.grey),
+                prefixIcon: const Icon(
+                  Icons.search,
+                  color: Colors.grey,
+                ),
+                fillColor: Colors.grey.shade800,
+                filled: true,
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30),
+                  borderSide: const BorderSide(color: Colors.transparent),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30),
+                  borderSide: const BorderSide(color: Colors.transparent),
+                ),
+              ),
+            ),
+            SizedBox(height: 20,),
+            Expanded(
+              child: ListView.builder(
+                padding: const EdgeInsets.only(top: 30),
+                itemCount: filteredNotes.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    margin: const EdgeInsets.only(bottom: 20),
+                    elevation: 3,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: ListTile(
+                        onTap: () async {
+                          final result = await Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (BuildContext context) =>
+                                  EditScreen(note: filteredNotes[index]),
+                            ),
+                          );
+                          if (result != null) {
+                            setState(() {
+                              int originalIndex =
+                              sampleNotes.indexOf(filteredNotes[index]);
+
+                              sampleNotes[originalIndex] = Note(
+                                  id: sampleNotes[originalIndex].id,
+                                  title: result[0],
+                                  content: result[1],
+                                  modifiedTime: DateTime.now());
+
+                              filteredNotes[index] = Note(
+                                  id: filteredNotes[index].id,
+                                  title: result[0],
+                                  content: result[1],
+                                  modifiedTime: DateTime.now());
+                            });
+                          }
+                        },
+                        title: RichText(
+                          maxLines: 3,
+                          overflow: TextOverflow.ellipsis,
+                          text: TextSpan(
+                              text: '${filteredNotes[index].title} \n',
+                              style: const TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 18,
+                                  height: 1.5),
+                              children: [
+                                TextSpan(
+                                  text: filteredNotes[index].content,
+                                  style: const TextStyle(
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.normal,
+                                      fontSize: 14,
+                                      height: 1.5),
+                                )
+                              ]),
+                        ),
+                        subtitle: Padding(
+                          padding: const EdgeInsets.only(top: 8.0),
+                          child: Text(
+                            'Edited: ${DateFormat('EEE MMM d, yyyy h:mm a').format(filteredNotes[index].modifiedTime)}',
+                            style: TextStyle(
+                                fontSize: 10,
+                                fontStyle: FontStyle.italic,
+                                color: Colors.grey.shade800),
+                          ),
+                        ),
+                        trailing: IconButton(
+                          onPressed: () async {
+                            final result = await confirmDialog(context);
+                            if (result != null && result) {
+                              deleteNote(index);
+                            }
+                          },
+                          icon: const Icon(
+                            Icons.delete,
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          final result = await Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (BuildContext context) => const EditScreen(),
+            ),
+          );
+
+          if (result != null) {
+            setState(() {
+              sampleNotes.add(Note(
+                  id: sampleNotes.length,
+                  title: result[0],
+                  content: result[1],
+                  modifiedTime: DateTime.now()));
+              filteredNotes = sampleNotes;
+            });
+          }
+        },
+        elevation: 10,
+        backgroundColor: Colors.grey.shade800,
+        child: const Icon(
+          Icons.add,
+          size: 38,
+        ),
+      ),
+    );
+  }
+
+  Future<dynamic> confirmDialog(BuildContext context) {
+    return showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          backgroundColor: Colors.grey.shade900,
+          title: const Text(
+            'Are you sure you want to delete?',
+            style: TextStyle(color: Colors.white),
+          ),
+          content: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context, true);
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.green.shade400,
+                ),
+                child: const SizedBox(
+                  width: 60,
+                  child: Text(
+                    'Yes',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context, false);
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.red.shade400,
+                ),
+                child: const SizedBox(
+                  width: 60,
+                  child: Text(
+                    'No',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
+
+class EditScreen extends StatefulWidget {
+  final Note? note;
+  const EditScreen({Key? key, this.note}) : super(key: key);
+
+  @override
+  State<EditScreen> createState() => _EditScreenState();
+}
+
+class _EditScreenState extends State<EditScreen> {
+  TextEditingController _titleController = TextEditingController();
+  TextEditingController _contentController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    if (widget.note != null) {
+      _titleController = TextEditingController(text: widget.note!.title);
+      _contentController = TextEditingController(text: widget.note!.content);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey.shade900,
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(16, 40, 16, 0),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                IconButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  icon: Container(
+                    width: 40,
+                    height: 40,
+                    decoration: BoxDecoration(
+                      color: Colors.grey.shade800.withOpacity(.8),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: const Icon(
+                      Icons.arrow_back_ios_new,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Expanded(
+              child: ListView(
+                children: [
+                  TextField(
+                    controller: _titleController,
+                    style: const TextStyle(color: Colors.white, fontSize: 30),
+                    decoration: const InputDecoration(
+                      border: InputBorder.none,
+                      hintText: 'Title',
+                      hintStyle: TextStyle(color: Colors.grey, fontSize: 30),
+                    ),
+                  ),
+                  TextField(
+                    controller: _contentController,
+                    style: const TextStyle(
+                      color: Colors.white,
+                    ),
+                    maxLines: null,
+                    decoration: const InputDecoration(
+                      border: InputBorder.none,
+                      hintText: 'Type something here',
+                      hintStyle: TextStyle(
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.pop(
+            context,
+            [_titleController.text, _contentController.text],
+          );
+        },
+        elevation: 10,
+        backgroundColor: Colors.grey.shade800,
+        child: const Icon(Icons.save),
+      ),
+    );
+  }
+}
+
+class Note {
+  int id;
+  String title;
+  String content;
+  DateTime modifiedTime;
+
+  Note({
+    required this.id,
+    required this.title,
+    required this.content,
+    required this.modifiedTime,
+  });
+}
+
+
+List<Note> sampleNotes = [
+  Note(
+    id: 0,
+    title: 'Specific',
+    content:
+    'I’m going to become a iOS developer for a leading software-as-a-service (SaaS) company like Spotify and work remotely.',
+    modifiedTime: DateTime(2023,8,1,34,5),
+  ),
+  Note(
+    id: 1,
+    title: 'Measurable',
+    content:
+    'I will apply to a minimum of 10 job applications within one months.',
+    modifiedTime: DateTime(2023,9,1,34,5),
+  ),
+  Note(
+    id: 2,
+    title: 'Achievable',
+    content:
+    'I’ve worked as a iOS engineer for two years in an office for a business firm, and I’m great at my job.',
+    modifiedTime: DateTime(2023,10,1,19,5),
+  ),
+  Note(
+    id: 3,
+    title: 'Relevant',
+    content:
+    'I want to collaborate with interesting leader, contribute to something innovative, and join a company with room for me to grow. Also, I want to travel long-term as a digital nomad.',
+    modifiedTime: DateTime(2023,10,20,20,9),
+  ),
+  Note(
+    id: 4,
+    title: 'Time-bound',
+    content:
+    'I will apply to 20 suitable job applications within two months by submitting 2 application per week.',
+    modifiedTime: DateTime(2023,10,21,13,9),
+  ),
+];
+
